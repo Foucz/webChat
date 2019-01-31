76379789f2fcad9f73ce9b683c54094f6898f9ef
Foucz <tomekandr@gmail.com>
2019-01-31 18:56:50 +0100

    Setup start script and heroku port

--- server/server.js
+++ server/server.js
@@ -32,6 +32,6 @@ io.on('connection',(socket)=>{
 
 });
 
-server.listen(3000, ()=>{
-  console.log('Server is up on port 3000');
+server.listen(port, ()=>{
+  console.log(`Server is up on port ${port}`);
 })
